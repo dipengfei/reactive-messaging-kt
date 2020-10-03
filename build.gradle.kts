bb26f5e6833122a93f741a539d0aa35ca9e448cb
dipengfei <pengfei.di@rakuten.com>
2020-10-03 18:15:31 +0800

    move spring version to parent project

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,8 @@
 plugins {
 
+    id("org.springframework.boot") version "2.3.4.RELEASE"
+    id("io.spring.dependency-management") version "1.0.10.RELEASE"
+
     kotlin("jvm") version "1.3.72" apply false
     kotlin("plugin.spring") version "1.3.72" apply false
 }
@@ -18,6 +21,13 @@ subprojects {
 
     apply {
         plugin("kotlin")
+    }
+
+    if (name != "rmkt-core") {
+        apply {
+            plugin("org.springframework.boot")
+            plugin("io.spring.dependency-management")
+        }
     }
 
 }
