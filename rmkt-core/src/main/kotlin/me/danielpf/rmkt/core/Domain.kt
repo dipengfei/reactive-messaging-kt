9231923218695027b74a4c04ebb2c5a8e65da50e
dipengfei <pengfei.di@rakuten.com>
2020-10-03 18:53:17 +0800

    add domain Product & ProductExchange

--- rmkt-core/src/main/kotlin/me/danielpf/rmkt/core/Domain.kt
+++ rmkt-core/src/main/kotlin/me/danielpf/rmkt/core/Domain.kt
@@ -1,2 +1,5 @@
 package me.danielpf.rmkt.core
 
+data class Product(val id: String, val name: String, val price: Double)
+
+data class ProductExchange(val product: Product, val localPrices: Map<String, Double>)
