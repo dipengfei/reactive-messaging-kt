2bafd3c439132261454ac6a917d521612a9f512a
dipengfei <pengfei.di@rakuten.com>
2020-10-03 17:35:39 +0800

    move kotlin version to parent project

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,23 @@
+plugins {
 
+    kotlin("jvm") version "1.3.72" apply false
+    kotlin("plugin.spring") version "1.3.72" apply false
+}
 
 allprojects {
+
     group = "me.danielpf"
 
     repositories {
         mavenCentral()
+    }
+
+}
+
+subprojects {
+
+    apply {
+        plugin("kotlin")
     }
 
 }
--- rmkt-producer/build.gradle.kts
+++ rmkt-producer/build.gradle.kts
@@ -3,8 +3,8 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     id("org.springframework.boot") version "2.3.4.RELEASE"
     id("io.spring.dependency-management") version "1.0.10.RELEASE"
-    kotlin("jvm") version "1.3.72"
-    kotlin("plugin.spring") version "1.3.72"
+    kotlin("jvm")
+    kotlin("plugin.spring")
 }
 
 version = "0.0.1-SNAPSHOT"
